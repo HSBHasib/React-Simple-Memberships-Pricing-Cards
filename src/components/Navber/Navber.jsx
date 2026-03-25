27e336219c1d8d95e184540e4d2964cf10b039b7
Hasib <hasibhsb19@gmail.com>
2026-03-25 13:33:58 +0600

    add responsive nav code

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Menu } from 'lucide-react';
 
 const Navber = () => {
   return (
-    <div className='flex justify-between px-6 py-2'>
-        <div>
-            <h1>NavberIcon</h1>
+    <div className='flex justify-between items-center px-6 py-2'>
+        <div className='flex gap-2'>
+            <Menu className='sm:hidden' size={24} color="#3d9bff" strokeWidth={1.75} />
+            <h1 className='font-semibold'>NavberIcon</h1>
         </div>
         
-        
-        <ul className='flex gap-8'>
+        <ul className='flex gap-8 text-sm max-[500px]:hidden'>
             <li>Home</li>
             <li>About</li>
             <li>Projects</li>
@@ -16,7 +17,7 @@ const Navber = () => {
         </ul>
 
         
-        <button className='rounded-lg bg-blue-400 px-4 py-1 shadow font-semibold hover:bg-blue-500 cursor-pointer'>SingUp</button>
+        <button className='rounded-lg bg-blue-400 px-4 py-1 shadow font-semibold hover:bg-blue-500 cursor-pointer '>SingUp</button>
         
     </div>
   )
