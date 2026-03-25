6ad47be64b10aa50cd148d2a0d629c034acc8521
Hasib <hasibhsb19@gmail.com>
2026-03-25 14:30:05 +0600

    add all code for nav section [nav section completed]

--- src/components/Navber/Navber.jsx
+++ src/components/Navber/Navber.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Menu } from 'lucide-react';
+import { X } from 'lucide-react';
 
 const navigationData = [
     {
@@ -25,24 +26,32 @@ const navigationData = [
 ]
 
 const Navber = () => {
+  const [open, setOpen] = useState(false);
+
+  const navInfo = navigationData.map(navData => <li className='hover:bg-blue-800 active:scale-50 rounded-md px-2 py-1 cursor-pointer' key={navData.id}>{navData.name}</li>);
+
   return (
-    <div className='flex justify-between items-center px-6 py-2'>
-        <div className='flex gap-2'>
-            <Menu className='sm:hidden' size={24} color="#3d9bff" strokeWidth={1.75} />
+    <div className='flex justify-between items-center px-6 py-3 '>
+        <div className='flex gap-1.5 list-none relative' onClick={() => setOpen(!open)}>
+            {open ? <X /> : <Menu className='sm:hidden' size={24} color="#3d9bff" strokeWidth={1.75} />}
+            <ul className={`text-sm absolute left-3 duration-700
+                    ${open ? 'top-6' : '-top-40'}`
+                }>
+                {navInfo}
+            </ul>
             <h1 className='font-semibold'>NavberIcon</h1>
         </div>
         
-        <ul className='flex gap-8 text-sm max-[500px]:hidden'>
+        <ul className='sm:flex gap-8 text-sm max-[500px]:hidden'>
             {
-                navigationData.map(navData => <li key={navData.id}>{navData.name}</li>)
+                navInfo
             }
         </ul>
 
-        
-        <button className='rounded-lg bg-blue-400 px-4 py-1 shadow font-semibold hover:bg-blue-500 cursor-pointer '>SingUp</button>
-        
+        <button className='rounded-lg bg-blue-400 px-4 py-1 shadow font-semibold hover:bg-blue-500 cursor-pointer '>SingUp</button>       
     </div>
   )
 }
 
 export default Navber
+
